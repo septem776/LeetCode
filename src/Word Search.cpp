5827ebe30250d37a05a8e53bdff7c35274edf560
septem776 <septem776@gmail.com>
2013-07-19 17:58:45 +0800

    optimize Word Search.cpp

--- src/Word Search.cpp
+++ src/Word Search.cpp
@@ -1,60 +1,38 @@
 class Solution {
 public:
+    bool dfs(string word, int dep, int maxdep, vector<vector<char>> &board, int x, int y, vector<vector<bool>> &visit, int dir[4][2]) {
+        if (dep == maxdep) return true;
+        int m = board.size();
+        int n = board[0].size();
+        for (int i = 0; i < 4; i++) {
+            int xx = x + dir[i][0];
+            int yy = y + dir[i][1];
+            if (xx >= 0 && xx < m && yy >= 0 && yy < n && board[xx][yy] == word[dep] && !visit[xx][yy]) {
+                visit[xx][yy] = true;
+                if (dfs(word, dep+1, maxdep, board, xx, yy, visit, dir)) return true;
+                visit[xx][yy] = false;
+            }
+        }
+    }
     bool exist(vector<vector<char> > &board, string word) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if(word.size() == 0)
-            return true;
-        if(board.empty())
-            return false;
-        
-        bool ret = false;
-        vector<vector<bool> > visited(board.size(), vector<bool>(board[0].size(), false));
-        for(int i=0; i<board.size(); i++)
-        {
-            for(int j=0; j<board[0].size(); j++)
-            {
-                if(word[0] == board[i][j])
-                {
-                    ret = search(board, word, 0, visited, i, j);
-                    if(ret)
-                        return true;
+        int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int m = board.size();
+        if (!m) return false;
+        int n = board[0].size();
+        if (!word.size()) return true;
+        vector<vector<bool>> visit(m, vector<bool>(n, false));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (word[0] == board[i][j]) {
+                    visit[i][j] = true;
+                    bool flag = false;
+                    if (dfs(word, 1, word.size(), board, i, j, visit, dir)) return true;
+                    visit[i][j] = false;
                 }
             }
         }
-        return ret;
-    }
-    
-    bool search(vector<vector<char> > board, string word, int dep, vector<vector<bool> > &visited, int posx, int posy)
-    {
-        if(dep == word.size())
-        {
-        	return true;
-        }
-        if(posx >= board.size() || posx < 0 || posy >= board[0].size() || posy < 0)
-        	return false;
-        
-    	if(board[posx][posy] != word[dep])
-    		return false;
-    	
-    	bool ret = false;
-    	if(!visited[posx][posy])
-    	{
-    		visited[posx][posy] = true;
-    		ret = search(board, word, dep+1, visited, posx+1, posy);
-        if(!ret)
-        		ret = search(board, word, dep+1, visited, posx, posy+1);
-        if(!ret)
-        		ret = search(board, word, dep+1, visited, posx, posy-1);
-    		if(!ret)
-    			ret = search(board, word, dep+1, visited, posx-1, posy);
-    		
-    		
-    		if(!ret)
-    			visited[posx][posy] = false;
-    	}
-    	
-    	return ret;
-    	
+        return false;
     }
 };
