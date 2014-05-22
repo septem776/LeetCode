db3bdfad0761d50f7bd7282c77003716d9eb790d
septem776 <septem776@gmail.com>
2014-05-22 22:25:54 +0800

    coding

--- src/Unique Binary Search Trees.cpp
+++ src/Unique Binary Search Trees.cpp
@@ -1,15 +1,37 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Septem on 3/29/14.
+//  Copyright (c) 2014 Septem. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
 class Solution {
 public:
     int numTrees(int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if(n == 0)
-            return 1;
-        int sum = 0;
-        for(int i=0; i<n; i++)
-        {
-            sum += numTrees(i) * numTrees(n-1-i);
+        vector<int> ret(n+1, 0);
+        ret[0] = 1;
+        ret[1] = 1;
+        for (int i=2; i<=n; i++) {
+            for (int j=0; j<i; j++) {
+                ret[i] += ret[j] * ret[i-j-1];
+            }
         }
-        return sum;
+        return ret[n];
     }
 };
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
