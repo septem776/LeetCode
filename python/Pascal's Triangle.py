0a6cce054026da769f05b42f98eec28fce7b937e
septem776 <septem776@gmail.com>
2014-06-27 10:28:12 +0800

    fix a bug

--- python/Pascal's Triangle.py
+++ python/Pascal's Triangle.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(numRows):
             l = []
             for j in range(i+1):
-                if j == 0 or j == i - 1:
+                if j == 0 or j == i:
                     l.append(1)
                 else:
                     l.append(ret[i-1][j-1] + ret[i-1][j])
